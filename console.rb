27783632b3deb7008fbf35da49d84693cf5f1df7
Sef Hill <sefhill@icloud.com>
2020-05-22 15:36:13 +0100

    Play loop implemented

--- console.rb
+++ console.rb
@@ -1,13 +1,36 @@
-#require 'pry'
+require 'pry'
 require_relative 'noughts_and_crosses'
 
 class ConsoleApp
 
     def initialize
         @game_coord = GameCoord.new
+        @turns = 0
         display_welcome_screen
         get_user_details
-        draw_grid
+    end
+
+    def play
+        #loop:
+        # draw grid with grid number / player symbols
+            # check for win (after first 3 turns)
+                #run win script
+                #exit loop
+            # check for full grid (is turns == 9 ?)
+                #run draw script
+                #exit loop
+        # get next player to choose an empty cell
+        # end loop
+        
+        keep_playing = true
+        until keep_playing == false
+            loop do
+                draw_grid
+                break if game_over?
+                player_takes_turn
+            end
+            keep_playing = play_again?
+        end            
     end
 
     def display_welcome_screen
@@ -19,7 +42,7 @@ class ConsoleApp
         player1 = gets.chomp
         @game_coord.set_first_player_name(player1)
 
-        puts "Now enter name for Player 2:"
+        puts "\nNow enter name for Player 2:"
         player2 = gets.chomp
         @game_coord.set_second_player_name(player2)
         #binding.pry
@@ -34,5 +57,25 @@ class ConsoleApp
         puts " 4 | 5 | 6"
         puts line_divider
         puts " 7 | 8 | 9"
+    end
+
+    def game_over?
+        return true
+    end
+
+    def play_again?
+        valid_input = false
+        user_input = ""
+
+        until valid_input
+            puts "\nPlay again? (y/n):"
+            user_input = gets.chomp.downcase
+            if user_input == "n" || user_input == "y"
+                valid_input = true
+            else
+                puts "\nPlease enter 'y' or 'n'"
+            end
+        end
+        user_input == "y"
     end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 require_relative "console"
 
 game = ConsoleApp.new
+game.play
